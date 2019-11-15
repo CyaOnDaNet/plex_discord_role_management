d585e819c3de36985dd07bff9ac340abcd593ab1
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2019-11-15 00:33:20 -0500

    Added a fetch user plex username command

--- index.js
+++ index.js
@@ -241,6 +241,32 @@ client.on('message', async message => {
     }
     message.channel.send({embed});
   }
+  else if (command === "users") {
+    if (!message.channel.guild.member(message.author).hasPermission('ADMINISTRATOR')) {
+      return message.channel.send('You do not have permissions to use `' + prefix + command + '`! It contains sensitive information.');
+    }
+
+    tautulli.get('get_users').then((result) => {
+      var users = result.response.data;
+      var userList = "\n";
+      for (var i = 0; i < users.length; i++) {
+        userList = userList + "> " + users[i].username + "\n";
+      }
+
+      embed = new Discord.RichEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .setDescription("Below is a list of users with access to the Plex Server:\n" + userList)
+        .setFooter("Fetched")
+        .setTimestamp(new Date())
+        .setColor(0x00AE86);
+      message.channel.send({embed});
+    }).catch((error) => {
+      console.log("Couldn't connect to Tautulli, check your settings.");
+      console.log(error);
+      // do we need to remove roles if this is the case? Maybe we don't...
+    });
+
+  }
 });
 
 var j = schedule.scheduleJob('*/30 * * * * *', function() {
