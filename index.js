a89d401409c6498690746c2b2f16b9682d8b59de
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2019-11-16 11:03:32 -0500

    Added a showlist command that grabs the currently airing list from Sonarr

--- index.js
+++ index.js
@@ -266,12 +266,41 @@ client.on('message', async message => {
     });
 
   }
-  else if (command === "test") {
-    tautulli.get('get_library_media_info').then((result) => {
-      console.log(result.response.data);
-      console.log(result);
-    }).catch((error) => {
-      console.log("Couldn't connect to Tautulli, check your settings.");
+  else if (command === "showlist" || command === "showslist") {
+    var url = config.sonarr_web_address;
+    if (!url.startsWith("http://") && !url.startsWith("http://")) {
+      // we need an http or https specified so we will asumme http
+      console.log("Please adjust your config.sonarr_web_address to include http:// or https://. Since it was not included, I am assuming it is http://");
+      url = "http://" + url;
+    }
+    if (!url.endsWith('/')) {
+      url = url + '/';
+    }
+    url = url + "api/series?apikey=" + config.sonarr_api_key;
+
+    fetch(url,  {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      var showsList = "\n";
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].status === "continuing") {
+          showsList = showsList + "> " + json[i].title + "\n";
+        }
+      }
+
+      embed = new Discord.RichEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .setDescription("Below is a list of shows on the Plex Server that are still continuing:\n" + showsList)
+        .setFooter("Fetched")
+        .setTimestamp(new Date())
+        .setColor(0x00AE86);
+      message.channel.send({embed});
+    })
+    .catch((error) => {
+      console.log("Couldn't connect to Sonarr, check your settings.");
+      message.channel.send("Couldn't connect to Sonarr, check your settings.");
       console.log(error);
       // do we need to remove roles if this is the case? Maybe we don't...
     });
