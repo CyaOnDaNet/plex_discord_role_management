7fe45710fc067861504a14b3337fcc7615d0ac7b
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2020-01-11 09:24:45 -0500

    More testing

--- commands/test.js
+++ commands/test.js
@@ -5,7 +5,29 @@ module.exports = {
 	adminCommand: true,
 	async execute(message, args, prefix, guildSettings, client, Discord, tautulli, config, fetch, exemptEmbedReactRoles, tautulliHook, sonarr) {
 
-		message.channel.send("To Do:\n • implement Admin role usage\n • write proper readme\n • finish notifications command:\n     • notifications refresh\n     • notifications reset\n     • notifications group\n     • notifications ungroup");
+		message.channel.send("To Do:\n • implement Admin role usage\n • write proper readme\n • finish notifications command:\n     • notifications refresh\n     • notifications reset\n");
 
+
+
+
+    /*
+		var args2 = message.content.slice(prefix.length).trim().split(/ +/g);
+		var ogCommand = args2.shift().toLowerCase();
+
+		if (args.length > 0) {
+			command = args.shift().toLowerCase();
+		} else {
+			command = "help";
+		}
+
+
+
+
+
+
+		if (command === "ungroup") {
+
+		}
+		*/
 	},
 };
