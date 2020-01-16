6e3779117fb9a0f17ef92d88b11e9a291954c5a2
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2020-01-16 15:43:06 -0500

    Fixed unintended sonarr error derived from earlier change

--- src/sonarr.js
+++ src/sonarr.js
@@ -4,8 +4,11 @@ const mainProgram = require("../index.js");
 module.exports = async(config) => {
   class sonarrService {
     constructor() {
-      var url = `${config.sonarr_ip}:${config.sonarr_port}/`;
-      this.baseURL = url;
+      this.baseURL = `${config.sonarr_ip}:${config.sonarr_port}/`;
+      if (!this.baseURL.startsWith("http://") && !this.baseURL.startsWith("https://")) {
+        // we need an http or https specified so we will asumme http
+        this.baseURL = "http://" + this.baseURL;
+      }
       this.sonarr_api_key = config.sonarr_api_key;
     }
 
