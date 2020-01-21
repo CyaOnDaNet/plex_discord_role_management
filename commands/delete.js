3faa2f91167666ff518f390bbfa635128e3adc01
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2020-01-20 19:51:08 -0500

    Created delete command to delete all Roles created by the bot

---
+++ commands/delete.js
@@ -0,0 +1,145 @@
+module.exports = {
+	name: 'delete',
+	description: 'Delete All Tracked Roles',
+	usage: '',
+	adminCommand: true,
+	async execute(message, args, prefix, guildSettings, client, Discord, config, fetch, exemptEmbedReactRoles, tautulli, sonarr) {
+		if (message.channel.guild.member(message.author).hasPermission('ADMINISTRATOR')) {
+			var emojiOptions = ['👍', '👎'];
+			const filter = (reaction, user) => emojiOptions.indexOf(reaction.emoji.name) != -1;
+
+			exemptEmbedReactRoles.push(`Delete All Tracked Roles`);
+			embed = new Discord.RichEmbed()
+				.setAuthor('Delete All Tracked Roles') //don't foget to edit exemptEmbedReactRoles above if name changes so it is ignored in index.js role react
+				.setDescription("⚠ **Warning** ⚠\nThis will delete all Discord Roles created by this bot, this is typically only something you want to do if removing this bot from the server. Click 👍 to confirm deletion.")
+				.setTimestamp(new Date())
+				.setColor(0x00AE86);
+
+			let sentMessage = await message.channel.send({embed});
+			sentMessage.react(emojiOptions[0])
+				.then(async () => {
+					sentMessage.awaitReactions(filter, { time: 15000 })
+						.then(async collected => {
+							var selectedEmojis = [];
+							collected.tap(selectedOptions => {
+								if (selectedOptions.users.get(message.author.id) != undefined) {
+									selectedEmojis.push(selectedOptions._emoji.name);
+								}
+							});
+							var setDescription = "Successfully removed the following Custom React Roles:\n(*Don't forget to delete the role in Discord, I don't manage custom roles!*)\n\n";
+							embed = new Discord.RichEmbed()
+								.setTimestamp(new Date())
+								.setColor(0x00AE86);
+
+							var count = 0;
+							var bypass = false;
+							for(let emojis of selectedEmojis) {
+								if (emojis == '👎') {
+									setDescription = "Cancelled, nothing deleted!";
+									bypass = true;
+								}
+							}
+							for(let emojis of selectedEmojis) {
+								if (emojis == '👍' && !bypass) {
+									setDescription = "Successfully removed all Roles created by this bot!\n\n(*Don't forget if you used a watching role or any custom roles that I do not manage those and did not delete them!*)";
+									//Iterate through database and delete all Roles
+
+									var deleteList = [];
+									var deleteNotificationList = [];
+									var deleteCustomList = [];
+
+									for (const showNotification of client.searchTvShowsNotificationSettings.iterate()) {
+				            if (showNotification.guild == message.guild.id && showNotification.roleID != null && showNotification.roleID != undefined && showNotification.roleID != "") {
+											if (await message.guild.roles.find(role => role.id === showNotification.roleID) != null) {
+												await message.guild.roles.find(role => role.id === showNotification.roleID).delete()
+													.then(async () => {
+														deleteList.push(showNotification.id);
+													})
+													.catch(console.error);
+											}
+											else {
+												deleteList.push(showNotification.id);
+											}
+				            }
+										if (showNotification.guild == message.guild.id && showNotification.groupRole != null && showNotification.groupRole != undefined && showNotification.groupRole != "") {
+											if (await message.guild.roles.find(role => role.id === showNotification.groupRole) != null) {
+												await message.guild.roles.find(role => role.id === showNotification.groupRole).delete()
+													.then(async () => {
+														deleteList.push(showNotification.id);
+													})
+													.catch(console.error);
+											}
+											else {
+												deleteList.push(showNotification.id);
+											}
+										}
+				          }
+
+									for (var i = 0; i < deleteList.length; i++) {
+										var tvShowsNotificationSettings = client.getTvShowsNotificationSettings.get(`${deleteList[i]}`);
+										tvShowsNotificationSettings.roleID = null;
+										tvShowsNotificationSettings.groupRole = null;
+										tvShowsNotificationSettings.is_group = null;
+										tvShowsNotificationSettings.groupName = null;
+										tvShowsNotificationSettings.exclude = null;
+										tvShowsNotificationSettings.include = null;
+										client.setTvShowsNotificationSettings.run(tvShowsNotificationSettings);
+									}
+
+									for (const notificationSetting of client.searchNotificationSettings.iterate()) {
+										if (notificationSetting.guild == message.guild.id && notificationSetting.roleID != null && notificationSetting.roleID != undefined && notificationSetting.roleID != "" && notificationSetting.category != "custom") {
+											if (await message.guild.roles.find(role => role.id === notificationSetting.roleID) != null) {
+												await message.guild.roles.find(role => role.id === notificationSetting.roleID).delete()
+													.then(async () => {
+														deleteNotificationList.push(notificationSetting.id);
+													})
+													.catch(console.error);
+											}
+											else {
+												deleteNotificationList.push(notificationSetting.id);
+											}
+				            }
+										if (notificationSetting.guild == message.guild.id && notificationSetting.roleID != null && notificationSetting.roleID != undefined && notificationSetting.roleID != "" && notificationSetting.category == "custom") {
+											deleteCustomList.push(notificationSetting.id);
+										}
+									}
+
+									for (var i = 0; i < deleteNotificationList.length; i++) {
+										var notificationSettings = client.getNotificationSettings.get(`${deleteNotificationList[i]}`);
+										notificationSettings.roleID = null;
+										client.setNotificationSettings.run(notificationSettings);
+									}
+
+									for (var i = 0; i < deleteCustomList.length; i++) {
+										client.deleteNotificationSettings.run(`${deleteCustomList[i]}`);    // delete a row based on id
+										guildSettings.customRoleCount = guildSettings.customRoleCount - 1;
+										client.setGuildSettings.run(guildSettings);
+										guildSettings = client.getGuildSettings.get(message.guild.id);
+									}
+
+								}
+								count++;
+							}
+
+							if (count === 0) {
+								embed = new Discord.RichEmbed()
+									.setDescription("Nothing selected in time, action cancelled!")
+									.setTimestamp(new Date())
+									.setColor(0x00AE86);
+							}
+							else {
+								embed.setDescription(setDescription);
+							}
+
+							sentMessage.edit({embed});
+						})
+						.catch(console.error);
+				})
+				.then(async () => { await sentMessage.react(emojiOptions[1]) })
+				.catch(() => console.error('One of the emojis failed to react.'));
+
+		} else {
+      return message.channel.send('You do not have permissions to use `' + prefix + 'delete`!');
+    }
+	},
+};
